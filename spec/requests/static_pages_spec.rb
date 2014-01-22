4af2c8864113a2a7741bcd933e101ca49daee2a3
Cindie <jcshanth2016@gmail.com>
2014-01-22 13:02:36 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,32 +1,42 @@
 require 'spec_helper'
 
 describe "Static pages" do
-
+  subject {page}
   describe "Home page" do
 
-  	it "should have the right title" do
-  	  visit '/static_pages/home'
-  	  expect(page).to have_title("Super Fun Awesome Test App | Home")
-  	end 
-  	
-    it "should have the content 'Spike App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Spike App')
-	end
+  	before {visit root_path}
+
+  	it { should have_content('Spike App')}
+  	it { should have_title(full_title(''))}
+  	it { should_not have_title('| Home')}
+
   end
 
   describe "Help page" do
-  	it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
-  	  expect(page).to have_content('Help')
-  	end 
+  	before{ visit help_path}
+
+  	it {should have_content ('Help')}
+  	it {should have_title(full_title('Help'))}
   end
 
   describe "About page" do
-  	it "should have then content 'About'" do
-  	  visit '/static_pages/about'
-  	  expect(page).to have_content('About')
-  	end
+  	before{ visit about_path}
+
+  	it {should have_content ('About')}
+  	it {should have_title(full_title('About Us'))}
   end
 
+  describe "Contact page" do
+    before{ visit contact_path}
+
+  	it {should have_content ('Contact')}
+  	it {should have_title(full_title('Contact'))}
+  end
+
+  describe "signup page" do
+  	before { visit signup_path}
+
+  	it {should have_content ('Sign up')}
+  	it {should have_title (full_title('Sign up'))}
+  end
 end
